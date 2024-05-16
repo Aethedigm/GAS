53f243c87dc941244f7d6e25af33771c6e697291
Stephen Sumpter <aethedigm@gmail.com>
2024-05-16 14:28:37 -0400

    Update gas_test.go

--- gas/tests/gas_test.go
+++ gas/tests/gas_test.go
@@ -15,6 +15,50 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
+func cleanup(t *testing.T) {
+	t.Cleanup(func() {
+		gasObj = new(gas.GAS)
+	})
+}
+
+func Test_retrieve_will_ignore_special_characters(t *testing.T) {
+	gasObj.AddResult("main", "val")
+	res := gasObj.GetResults("ma~in")
+
+	for x := range res.Results {
+		if res.Results[x].Word == "main" {
+			return
+		} else {
+			t.Error("Should have found 'main'")
+		}
+	}
+
+	t.Error("Should have found 'main'")
+	cleanup(t)
+}
+
+func Test_insert_will_not_add_special_characters(t *testing.T) {
+	gasObj.AddResult("specia!l", "a&b")
+	res := gasObj.GetResults("special")
+
+	for x := range res.Results {
+		if res.Results[x].Word == "special" {
+			for y := range res.Results[x].Values {
+				if res.Results[x].Values[y] == "ab" {
+					return
+				} else {
+					t.Error("Expected 'ab' got ", res.Results[x].Values[y])
+				}
+			}
+		} else {
+			t.Error("Non-inserted value found")
+		}
+	}
+
+	t.Error("Should have found 'special'")
+	cleanup(t)
+}
+
 func Test_insert(t *testing.T) {
 	gasObj.AddResult("key", "value")
 	res := gasObj.GetResults("key")
@@ -34,6 +78,7 @@ func Test_insert(t *testing.T) {
 	}
 
 	t.Error("Should have found the key and value")
+	cleanup(t)
 }
 
 func Test_retrieve(t *testing.T) {
@@ -55,4 +100,5 @@ func Test_retrieve(t *testing.T) {
 	}
 
 	t.Error("Should have found the value: retrieve")
+	cleanup(t)
 }
