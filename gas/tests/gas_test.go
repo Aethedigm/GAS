a57a792b8b15c7e46fe42dfc612b7dd809a11eef
Stephen Sumpter <aethedigm@gmail.com>
2024-05-16 09:15:05 -0400

    Update gas_test.go

--- gas/tests/gas_test.go
+++ gas/tests/gas_test.go
@@ -2,12 +2,20 @@ package tests
 
 import (
 	"main/gas"
+	"os"
 	"testing"
 )
 
-func Test_insert(t *testing.T) {
-	gasObj := new(gas.GAS)
+var gasObj *gas.GAS
 
+func TestMain(m *testing.M) {
+	gasObj = new(gas.GAS)
+
+	code := m.Run()
+	os.Exit(code)
+}
+
+func Test_insert(t *testing.T) {
 	gasObj.AddResult("key", "value")
 	res := gasObj.GetResults("key")
 
@@ -26,4 +34,25 @@ func Test_insert(t *testing.T) {
 	}
 
 	t.Error("Should have found the key and value")
+}
+
+func Test_retrieve(t *testing.T) {
+	gasObj.AddResult("retrieve", "true")
+
+	rr := gasObj.GetResults("retrieve")
+	if len(rr.Results) < 1 {
+		t.Error("Should have found the key")
+	}
+
+	for x := range rr.Results {
+		if rr.Results[x].Word == "retrieve" {
+			for y := range rr.Results[x].Values {
+				if rr.Results[x].Values[y] == "true" {
+					return
+				}
+			}
+		}
+	}
+
+	t.Error("Should have found the value: retrieve")
 }
